2e5a246696a19573c46748a3e2a11340ed81f822
JyothiKumar03 <djdummala@gmail.com>
2026-05-10 00:15:35 +0530

    fix: layout issue fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,7 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <script dangerouslySetInnerHTML={{ __html: themeInitScript }} />
       </head>
       <body
